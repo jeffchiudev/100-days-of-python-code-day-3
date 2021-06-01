6a9c6e7a993e5e0b13e269ffb6eb560208c32b35
Jeff Chiu <jwchiu82@gmail.com>
2021-05-31 21:08:28 -0700

    make slight changes to 3.4 to match lesson

--- 3-4.py
+++ 3-4.py
@@ -10,16 +10,23 @@ bill = 0
 
 if size == "S":
   bill += 15
-  if add_pepperoni == "Y":
-    bill += 2
-if size == "M":
+  # if add_pepperoni == "Y":
+  #   bill += 2
+elif size == "M":
   bill += 20
-  if add_pepperoni == "Y":
-    bill += 3
-if size == "L":
+  # if add_pepperoni == "Y":
+  #   bill += 3
+else:
   bill += 25
-  if add_pepperoni == "Y":
+  # if add_pepperoni == "Y":
+  #   bill += 3
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else: 
     bill += 3
+    
 if extra_cheese == "Y":
   bill += 1
 
