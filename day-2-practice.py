dc87222e422779665f4597686e37a5d7802ac21f
Jeff Chiu <jwchiu82@gmail.com>
2021-05-31 20:26:21 -0700

    complete exercise 3-2

--- day-2-practice.py
+++ day-2-practice.py
@@ -1,9 +1,21 @@
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-if number % 2 == 0:
-  print("This is an even number.")
+
+BMI = round(weight / (height ** 2))
+
+if BMI < 18.5:
+  print(f"Your BMI is {BMI}, you are underweight")
+elif BMI < 25:
+  print(f"Your BMI is {BMI}, you are normal weight")
+elif BMI < 30:
+  print(f"Your BMI is {BMI}, you are slightly overweight")
+elif BMI < 35:
+  print(f"Your BMI is {BMI}, you are obese")
 else:
-  print("This is an odd number.")
+  print(f"Your BMI is {BMI}, you are clinically obese")
+  
+
